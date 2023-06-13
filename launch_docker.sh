4226b4bf0382a10a38f902033692a67f7fbcc4fa
franchesoni <marchesoniacland@gmail.com>
2023-06-13 17:59:21 +0200

    prepare docker

--- launch_docker.sh
+++ launch_docker.sh
@@ -1,1 +1,3 @@
-docker run -p 8000:8000 -v C:\Users\basti\Desktop\Stage\kagglecatsanddogs3367a:/images binaryannotation --gpus all --pull always franchesoni/cd
+DATADIR=$1
+# docker run -p 8000:8000 -$DATADIRIR:/archive --gpus all --pull always franchesonbinannnn:0.1 
+docker run -p 8000:8000 -v $DATADIR:/archive --pull always franchesoni/binann:0.1 
