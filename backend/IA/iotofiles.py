e465a04c3c30610aa83f33ceee11c589c339b44c
franchesoni <marchesoniacland@gmail.com>
2023-06-17 22:29:20 +0200

    use pickle instead of torch

--- backend/IA/iotofiles.py
+++ backend/IA/iotofiles.py
@@ -35,31 +35,7 @@ def safely_read(file: str | Path) -> Any:
     return ret
 
 def safely_save_torch(net: torch.nn.Module, path: str | Path):
-    with open(path, 'wb') as f:
-        # Apply an exclusive lock on the file descriptor
-        fcntl.flock(f.fileno(), fcntl.LOCK_EX)
-        # Write the data using pickle
-        torch.save(net.state_dict(), f)
-        # Release the lock on the file descriptor
-        fcntl.flock(f.fileno(), fcntl.LOCK_UN)
+    safely_write(path, net)
 
 def safely_load_torch(net: torch.nn.Module, path: str | Path) -> torch.nn.Module:
-    tries = 0
-    while True:
-        # try:
-            with open(path, 'rb') as f:
-                # Apply a shared lock on the file descriptor
-                fcntl.flock(f.fileno(), fcntl.LOCK_SH)
-                # Read the data using pickle
-                net.load_state_dict(torch.load(f))
-                # Release the lock on the file descriptor
-                fcntl.flock(f.fileno(), fcntl.LOCK_UN)
-            break
-        # except (EOFError, RuntimeError, OSError) as e:
-        #     tries += 1
-        #     if tries > 999:
-        #         print('error loading model, giving up')
-        #         raise e
-        #     print('error loading model, trying again...')
-        #     time.sleep(0.1)
-    return net
+    return safely_read(path)
