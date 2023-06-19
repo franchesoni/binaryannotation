4328e40cf82d2797be892364c2991d9974b935e7
franchesoni <marchesoniacland@gmail.com>
2023-06-19 11:39:14 +0200

    update launch script

--- launch_docker.sh
+++ launch_docker.sh
@@ -1,3 +1,9 @@
 DATADIR=$1
-# docker run -p 8000:8000 -p 6066:6066 -v $DATADIR:/archive --pull always franchesoni/binann:0.1 
+# raise error if no argument is given
+if [ -z "$DATADIR" ]
+then
+    echo "No argument supplied! Please add the path to your folder with images."
+    exit 1
+fi
+
 docker run -p 8000:8000 -p 6066:6066 -v $DATADIR:/archive --gpus all --pull always franchesoni/binann:0.1 
